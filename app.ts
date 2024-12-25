20d87799666bedd2a1f9a0361ed3e8cc5c84b4f8
Ashraf Sonde <ashrafsonde@gmail.com>
2024-12-25 15:44:09 +0530

    update

--- app.ts
+++ app.ts
@@ -1,1 +1,27 @@
-console.log("Test console log here ...");
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  logoTodo(id, title, finished);
+});
+
+const logoTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The todo ID is ${id}
+    Has title ${title}
+    Finised: ${completed}
+  `);
+};
